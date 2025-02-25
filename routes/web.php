5d7e214baf9141f0fa7da9b4363efdf439a4ee67
khokoon <ferdush.ahmed63@gmail.com>
2025-02-25 15:19:26 +0600

    correct route define

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ProductController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,12 +22,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Authentication Routes (Laravel 10 uses Laravel Breeze, Jetstream or Fortify for auth scaffolding)
 Auth::routes();
 
+// Home Route
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-Route::resource([
-    'roles' => RoleController::class,
-    'users' => UserController::class,
-    'products' => ProductController::class,
-]);
+// Resource Routes (Laravel 10 compatible)
+Route::middleware(['auth'])->group(function () {
+    Route::resources([
+        'roles' => RoleController::class,
+        'users' => UserController::class,
+        'products' => ProductController::class,
+    ]);
+});
