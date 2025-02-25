6fe5914e394c4f23b2a6417bd2611b6ecb2844f5
khokoon <ferdush.ahmed63@gmail.com>
2025-02-25 14:32:01 +0600

    update AuthServiceProvider

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
 
 class PermissionSeeder extends Seeder
 {
@@ -13,5 +14,22 @@ class PermissionSeeder extends Seeder
     public function run(): void
     {
         //
-    }
+        $permissions = [
+            'create-role',
+            'edit-role',
+            'delete-role',
+            'create-user',
+            'edit-user',
+            'delete-user',
+            'create-product',
+            'edit-product',
+            'delete-product'
+
+        ];
+
+        // Looping and Inserting Array's Permissions into Permission Table
+        foreach($permissions as $permission){
+            Permission::create(['name' => $permission]);
+        }
+
 }
