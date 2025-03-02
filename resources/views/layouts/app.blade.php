32c0d8a90ef24389cfda5856bd6506536827cb56
khokoon <ferdush.ahmed63@gmail.com>
2025-03-02 11:57:08 +0600

    blade files added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,12 +7,12 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Simple Laravel 10 User Roles and Permissions - AllPHPTricks.com</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
@@ -21,7 +21,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    AllPHPTricks.com
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -49,6 +49,15 @@
                                 </li>
                             @endif
                         @else
+                            @canany(['create-role', 'edit-role', 'delete-role'])
+                                <li><a class="nav-link" href="{{ route('roles.index') }}">Manage Roles</a></li>
+                            @endcanany
+                            @canany(['create-user', 'edit-user', 'delete-user'])
+                                <li><a class="nav-link" href="{{ route('users.index') }}">Manage Users</a></li>
+                            @endcanany
+                            @canany(['create-product', 'edit-product', 'delete-product'])
+                                <li><a class="nav-link" href="{{ route('products.index') }}">Manage Products</a></li>
+                            @endcanany
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
@@ -73,7 +82,34 @@
         </nav>
 
         <main class="py-4">
-            @yield('content')
+            <div class="container">
+                <div class="row justify-content-center mt-3">
+                    <div class="col-md-12">
+
+                        @if ($message = Session::get('success'))
+                            <div class="alert alert-success text-center" role="alert">
+                                {{ $message }}
+                            </div>
+                        @endif
+
+                        <h3 class="text-center mt-3 mb-3">Simple Laravel 10 User Roles and Permissions - <a href="https://www.allphptricks.com/">AllPHPTricks.com</a></h3>
+                        @yield('content')
+
+                        <div class="row justify-content-center text-center mt-3">
+                            <div class="col-md-12">
+                                <p>Back to Tutorial:
+                                    <a href="https://www.allphptricks.com/simple-laravel-10-user-roles-and-permissions/"><strong>Tutorial Link</strong></a>
+                                </p>
+                                <p>
+                                    For More Web Development Tutorials Visit: <a href="https://www.allphptricks.com/"><strong>AllPHPTricks.com</strong></a>
+                                </p>
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
         </main>
     </div>
 </body>
--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,3 +1,41 @@
 <div>
     <!-- Nothing in life is to be feared, it is only to be understood. Now is the time to understand more, so that we may fear less. - Marie Curie -->
 </div>
+@extends('layouts.app')
+
+@section('content')
+
+<div class="row justify-content-center">
+    <div class="col-md-8">
+
+        <div class="card">
+            <div class="card-header">
+                <div class="float-start">
+                    Product Information
+                </div>
+                <div class="float-end">
+                    <a href="{{ route('products.index') }}" class="btn btn-primary btn-sm">&larr; Back</a>
+                </div>
+            </div>
+            <div class="card-body">
+
+                    <div class="row">
+                        <label for="name" class="col-md-4 col-form-label text-md-end text-start"><strong>Name:</strong></label>
+                        <div class="col-md-6" style="line-height: 35px;">
+                            {{ $product->name }}
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <label for="description" class="col-md-4 col-form-label text-md-end text-start"><strong>Description:</strong></label>
+                        <div class="col-md-6" style="line-height: 35px;">
+                            {{ $product->description }}
+                        </div>
+                    </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
